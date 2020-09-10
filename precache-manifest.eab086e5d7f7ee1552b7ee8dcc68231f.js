6087e08551f16b10c29b0f6ba1b97ba04d19f3ca
sivamuruganandam-coder <sivamuruganandam@gmail.com>
2020-09-10 10:39:15 +0530

    Updates

---
+++ precache-manifest.eab086e5d7f7ee1552b7ee8dcc68231f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5717e44b70fd0d683deb3e54ab8ba374",
+    "url": "/cooking-recipe.git/index.html"
+  },
+  {
+    "revision": "a7d43db0839cb6b2d19b",
+    "url": "/cooking-recipe.git/static/css/main.57381e8e.chunk.css"
+  },
+  {
+    "revision": "b100382696365e3d717a",
+    "url": "/cooking-recipe.git/static/js/2.52767171.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cooking-recipe.git/static/js/2.52767171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7d43db0839cb6b2d19b",
+    "url": "/cooking-recipe.git/static/js/main.c36a417c.chunk.js"
+  },
+  {
+    "revision": "78f72f75fdfaaa21c041",
+    "url": "/cooking-recipe.git/static/js/runtime-main.430964d2.js"
+  },
+  {
+    "revision": "5d0373a3a48f49c76acb76447e6884bf",
+    "url": "/cooking-recipe.git/static/media/2184693.5d0373a3.jpg"
+  }
+]);
