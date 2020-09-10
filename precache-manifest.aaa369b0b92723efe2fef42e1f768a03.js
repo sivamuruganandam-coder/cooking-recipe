e5d3b56cb2d96be588710db8bea8af8258a99950
sivamuruganandam-coder <sivamuruganandam@gmail.com>
2020-09-10 12:02:39 +0530

    Updates

---
+++ precache-manifest.aaa369b0b92723efe2fef42e1f768a03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f95313592f8579550a8ed238ee6600",
+    "url": "/cooking-recipe/index.html"
+  },
+  {
+    "revision": "ece9d2193c7864615bcc",
+    "url": "/cooking-recipe/static/css/main.e0332c5e.chunk.css"
+  },
+  {
+    "revision": "b100382696365e3d717a",
+    "url": "/cooking-recipe/static/js/2.52767171.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cooking-recipe/static/js/2.52767171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ece9d2193c7864615bcc",
+    "url": "/cooking-recipe/static/js/main.80a23fcf.chunk.js"
+  },
+  {
+    "revision": "c4d9ffe2bc069758181d",
+    "url": "/cooking-recipe/static/js/runtime-main.7467cef4.js"
+  },
+  {
+    "revision": "5d0373a3a48f49c76acb76447e6884bf",
+    "url": "/cooking-recipe/static/media/2184693.5d0373a3.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cooking-recipe.git/precache-manifest.eab086e5d7f7ee1552b7ee8dcc68231f.js"
+  "/cooking-recipe/precache-manifest.aaa369b0b92723efe2fef42e1f768a03.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cooking-recipe.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cooking-recipe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
